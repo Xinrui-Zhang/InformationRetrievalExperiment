acea9eb61053c69bf57c138da2bea6eb43dee840
“Xinrui-Zhang <871024871@qq.com>
2021-05-07 19:23:17 +0800

    字典构建

--- IRproject/src/dataProcessor.java
+++ IRproject/src/dataProcessor.java
@@ -2,7 +2,7 @@ import org.json.*;
 import java.io.*;
 
 /**
- * @author 87102
+ * @author 张心睿
  * @date 2021/5/6
  * @description
  */
@@ -16,21 +16,24 @@ public class dataProcess {
      * @param path
      * @return void
      **/
+    private static Poet[] poets;
     public static void readFile(String path) throws IOException {
         char[] cbuf = new char[1000000000];
         InputStreamReader input =new InputStreamReader(new FileInputStream(new File(path)));
         int len =input.read(cbuf);
         String text =new String(cbuf,0,len);
-        //1.构造一个json对象
+        //构建json对象
         JSONObject obj = new JSONObject(text.substring(text.indexOf("{")));
         System.out.println("groupID"+obj.getString("groupID"));
+        //获取古诗数组
         JSONArray arr = obj.getJSONArray("poets");
-        Poet[] poets = new Poet[arr.length()];
+        poets = new Poet[arr.length()];
         for(int i=0;i<arr.length();i++)
         {
             JSONObject subObj = arr.getJSONObject(i);
             JSONArray subArr = subObj.getJSONArray("paragraphs");
             StringBuilder sentence = new StringBuilder();
+            //获取诗词内容句子数组
             for(int j=0;j< subArr.length();j++){
                 sentence.append(subArr.getString(j));
             }
@@ -39,8 +42,20 @@ public class dataProcess {
         }
 
     }
+
+    public static void writeFile() throws IOException {
+        for (Poet poet : poets) {
+            File newFile = new File(".\\dataset\\poets\\" + poet.getTitle()+".txt");
+            if (!newFile.exists())
+                newFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(newFile);
+            fos.write(poet.getContent().getBytes());
+            fos.close();
+        }
+    }
     public static void main(String[] args) throws IOException {
         String path =".\\dataset\\poet.tang.1000.json";
         readFile(path);
+        writeFile();
     }
 }
